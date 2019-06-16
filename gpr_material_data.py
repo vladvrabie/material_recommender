7d10606777a84a4e6847f5610acc8d485e58fbe8
Vlad Vrabie <vladvrabie97@gmail.com>
2019-06-16 20:30:36 +0300

    Added utility function in gpr data for writing to disk (thank you blender).

--- gpr_material_data.py
+++ gpr_material_data.py
@@ -3,6 +3,8 @@ from bpy.props import CollectionProperty, IntProperty
 from bpy.props import FloatVectorProperty, StringProperty
 from bpy.types import PropertyGroup
 from . frame_id_property import FrameIdProperty
+import os
+import tempfile
 
 
 class GPRMaterialData(PropertyGroup):
@@ -48,6 +50,10 @@ class GPRMaterialData(PropertyGroup):
         ''' Utility function to get the texture of a frame. '''
         return bpy.data.textures[self.frames_ids[frame_index].id]
 
+    def frame_image(self, frame_index):
+        ''' Utility function to get the image of a frame. '''
+        return bpy.data.images[self.frames_ids[frame_index].id]
+
     def load_from_folder(self, path, frames_count=1, prefix='', extension=''):
         ''' prefix + "frame" + str(i).zfill(4) + extension '''
         for i in range(0, frames_count):
@@ -59,5 +65,24 @@ class GPRMaterialData(PropertyGroup):
             bpy.data.textures.new(name=file_name, type='IMAGE')
             bpy.data.textures[file_name].image = bpy.data.images[file_name]
 
-    def load_from_memory(list_of_images):
+    def load_from_memory(self, list_of_images):
         pass
+
+    def save_to_disk(self, path='', file_format='PNG'):
+        if path == '':
+            path = tempfile.gettempdir()
+            path = os.path.join(path, 'material_recommender')
+            if not os.path.isdir(path):
+                os.mkdir(path)
+
+        frames_names = []
+        for i in range(len(self.frames_ids)):
+            frame = self.frame_image(i)
+            frame.file_format = file_format
+            file_name = 'frame' + str(i).zfill(4) + '.' + file_format.lower()
+            frame_path = os.path.join(path, file_name)
+            frames_names.append(file_name)
+            if os.path.isfile(frame_path):
+                os.remove(frame_path)
+            frame.save_render(frame_path)
+        return (path, frames_names)
