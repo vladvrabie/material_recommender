94027b3d5b35cdf838f81e47dc7e6cb434ed8125
Vlad Vrabie <vladvrabie97@gmail.com>
2019-06-16 20:31:37 +0300

    Implemented visualizing materials in the video sequencer. Updated generator.

--- export_to_vse_operator.py
+++ export_to_vse_operator.py
@@ -1,4 +1,5 @@
 from bpy.types import Operator
+import os
 
 
 class ExportToVSEOperator(Operator):
@@ -8,4 +9,24 @@ class ExportToVSEOperator(Operator):
         'Sequencer to be visualised as an animation.'
 
     def execute(self, context):
+        gpr_material = context.scene.gpr_materials.selected
+        frames_path, frames_names = gpr_material.save_to_disk()
+
+        # context.scene.sequence_editor_create()  # good? bad?
+        sequence_name = 'RecommenderStrip'
+        sequences = context.scene.sequence_editor.sequences
+
+        if sequence_name in sequences:
+            sequences.remove(sequences[sequence_name])
+
+        sequence = sequences.new_image(
+            name=sequence_name,
+            filepath=os.path.join(frames_path, frames_names[0]),
+            channel=1,
+            frame_start=1,
+        )
+
+        for i in range(1, len(frames_names)):
+            sequence.elements.append(frames_names[i])
+
         return {'FINISHED'}
--- gpr_list_generator.py
+++ gpr_list_generator.py
@@ -20,15 +20,15 @@ class GRPListGenerator(Operator):
         # return values would be one or more ndarrays with frames
         # gpr_data.load_from_memory(list of frames)
 
-        # folder2 = ('C:\\Users\\vladv\\Desktop\\test\\18_1_7__0\\', 'a_')
+        folder1 = ('C:\\Users\\vladv\\Desktop\\test\\h200\\', 'b_')
+        folder2 = ('C:\\Users\\vladv\\Desktop\\test\\0_14_12__0\\', 'a_a_')
         # folder3 = ('C:\\Users\\vladv\\Desktop\\test\\18_6_40__0\\', 'b_')
-        # folder1 = ('C:\\Users\\vladv\\Desktop\\test\\h200\\', 'c_')
-        # sel = random.sample((folder1, folder2, folder3), 1)[0]
-        # current_material.material_id = 'test'
+        sel = random.sample((folder1, folder2), 1)[0]
+        current_material.material_id = sel[1]
         current_material.load_from_folder(
-            'C:\\Users\\vladv\\Desktop\\test\\h200\\',
+            sel[0],
             frames_count=26,
-            prefix='',
+            prefix=sel[1],
             extension='.png'
         )
 
