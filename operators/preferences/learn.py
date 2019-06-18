de63a90d7b9039873c5d92058b2038910994b3e7
Vlad Vrabie <vladvrabie97@gmail.com>
2019-06-18 22:26:59 +0300

    Added hardcoded data for search tab.

--- operators/preferences/learn.py
+++ operators/preferences/learn.py
@@ -19,7 +19,25 @@ class LearnPreferencesOperator(Operator):
         import bpy
         var = context.scene.search_properties.latent_space_image_id
         bpy.data.textures.new(name=var, type='IMAGE')
-        bpy.data.textures[var].image = bpy.data.images['b_frame0000.png']
+        props = context.scene.search_properties
+        props.materials.collection.add()
+        props.materials.index = 0
+        props.materials.collection[-1].frames_ids.add()
+        props.materials.collection[-1].frames_ids.add()
+        props.materials.collection[-1].frames_ids.add()
+
+        try:
+            bpy.data.textures[var].image = bpy.data.images['b_frame0000.png']
+            props.materials.collection[-1].frames_ids[0].id = 'b_frame0000.png'
+            props.materials.collection[-1].frames_ids[1].id = 'b_frame0001.png'
+            props.materials.collection[-1].frames_ids[2].id = 'b_frame0002.png'
+
+        except:
+            bpy.data.textures[var].image = bpy.data.images['a_a_frame0000.png']
+            props.materials.collection[-1].frames_ids[0].id = 'a_a_frame0000.png'
+            props.materials.collection[-1].frames_ids[1].id = 'a_a_frame0001.png'
+            props.materials.collection[-1].frames_ids[2].id = 'a_a_frame0002.png'
+
         # END MOCK
 
         return {'FINISHED'}
