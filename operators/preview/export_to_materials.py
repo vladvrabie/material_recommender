db0dfaec8fa99b38692ba84850f70543675f2a82
Vlad Vrabie <vladvrabie97@gmail.com>
2019-06-22 13:39:40 +0300

    Export to materials: populated uber with shaders, not linked

--- operators/preview/export_to_materials.py
+++ operators/preview/export_to_materials.py
@@ -36,4 +36,50 @@ class ExportToMaterialsOperator(Operator):
 
     def _create_uber_material(self, context):
         uber_material = bpy.data.materials.new('Uber')
+        uber_material.use_nodes = True
+        nodes = uber_material.node_tree.nodes
+        nodes.clear()
+
+        glossy_bsdf_id = 'ShaderNodeBsdfGlossy'
+        glossy1 = nodes.new(glossy_bsdf_id)
+        glossy1.name = 'Glossy1 BSDF'
+        glossy1.location = (-130.0, 340.0)
+
+        glossy2 = nodes.new(glossy_bsdf_id)
+        glossy2.name = 'Glossy2 BSDF'
+        glossy2.location = (-130.0, 180.0)
+
+        glass_bsdf_id = 'ShaderNodeBsdfGlass'
+        glass = nodes.new(glass_bsdf_id)
+        glass.name = 'Glass BSDF'
+        glass.location = (-130.0, 20.0)
+
+        translucent_bsdf_id = 'ShaderNodeBsdfTranslucent'
+        translucent = nodes.new(translucent_bsdf_id)
+        translucent.name = 'Translucent BSDF'
+        translucent.location = (-130.0, -160.0)
+
+        mix_id = 'ShaderNodeMixShader'
+        mix_glossies = nodes.new(mix_id)
+        mix_glossies.name = 'Mix Glossies Shader'
+        mix_glossies.location = (200.0, 250.0)
+
+        mix_lucids = nodes.new(mix_id)
+        mix_lucids.name = 'Mix Lucids Shader'
+        mix_lucids.location = (200.0, -10.0)
+
+        mix_mixers = nodes.new(mix_id)
+        mix_mixers.name = 'Mix Mixers Shader'
+        mix_mixers.location = (500.0, 150.0)
+
+        volume_abs_id = 'ShaderNodeVolumeAbsorption'
+        volume_abs = nodes.new(volume_abs_id)
+        volume_abs.name = 'Volume Absorbtion Shader'
+        volume_abs.location = (500.0, 0.0)
+
+        material_output_id = 'ShaderNodeOutputMaterial'
+        material_output = nodes.new(material_output_id)
+        material_output.name = 'Material Output'
+        material_output.location = (800.0, 60.0)
+
         return uber_material
