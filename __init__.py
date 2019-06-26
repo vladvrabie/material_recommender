7c8fd011ac23d943be3aa7988947dbf8459941e3
Vlad Vrabie <vladvrabie97@gmail.com>
2019-06-26 11:27:36 +0300

    Created gms (= gaussian material synthesis) package.

--- __init__.py
+++ __init__.py
@@ -11,6 +11,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+import os
+import sys
+sys.path.append(os.path.abspath('.'))
+
 import bpy
 from bpy.props import PointerProperty
 from bpy.types import Scene
